023cbad04bfa103276f4aab485f22864dfd44473
Alex Zhuang <azhuang3@uci.edu>
2025-01-25 16:45:31 -0500

    maybe finished?

--- frontend/src/flaskComms.jsx
+++ frontend/src/flaskComms.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/style.css";
-
+import "@react-sigma/layout-noverlap";
+import { useLayoutNoverlap } from "@react-sigma/layout-noverlap";
 const sigmaStyle = { height: "1080px", width: "1080px" };
-
+const { positions, assign } = useLayoutNoverlap();
 // Component that load the graph
 export const LoadGraph = (username) => {
     const loadGraph = useLoadGraph();
@@ -16,10 +17,14 @@ export const LoadGraph = (username) => {
         .then(data => setData(data))
         .catch(error => console.log(error))
         
-        graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node", color: "#FA4F40" });
+        for(let key in data){
+          graph.addNode(key, { x: 0, y: 0, size: 15, label: key, color: "#FA4F40" });
+        }
+        
         loadGraph(graph);
+        assign();
         console.log(data);
-    }, [loadGraph]);
+    }, [assign,loadGraph]);
     return null;
 }
 
