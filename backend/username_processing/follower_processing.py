d2ee55bc8153b451a7fcca0e8fb2e0a3cbc22b0c
Steven Jongyun Yi <sjyi1@uci.edu>
2025-01-25 13:38:43 -0800

    prepped follower_processing

--- backend/username_processing/follower_processing.py
+++ backend/username_processing/follower_processing.py
@@ -1,5 +1,10 @@
 from instagrapi import Client
+from instagrapi.exceptions import LoginRequired
+import logging
 
+
+USERNAME = "steveyivicious"
+PASSWORD = "1028571DV"
 """
 Remember: A connection between nodes is only made when people follow each other
 
@@ -18,4 +23,74 @@ def example(a, b):
     """
     pass
 
+
+class CentralAccount:
+    central_account = Client()
+
+    def login_user():
+        """
+        Attempts to login to Instagram using either the provided session information
+        or the provided username and password.
+        """
+        logger = logging.getLogger()
+
+        central_account = Client()
+        session = central_account.load_settings("session.json")
+
+        login_via_session = False
+        login_via_pw = False
+
+        if session:
+            try:
+                central_account.set_settings(session)
+                central_account.login(USERNAME, PASSWORD)
+
+                # check if session is valid
+                try:
+                    central_account.get_timeline_feed()
+                except LoginRequired:
+                    logger.info("Session is invalid, need to login via username and password")
+
+                    old_session = central_account.get_settings()
+
+                    # use the same device uuids across logins
+                    central_account.set_settings({})
+                    central_account.set_uuids(old_session["uuids"])
+
+                    central_account.login(USERNAME, PASSWORD)
+                login_via_session = True
+            except Exception as e:
+                logger.info("Couldn't login user using session information: %s" % e)
+
+        if not login_via_session:
+            try:
+                logger.info("Attempting to login via username and password. username: %s" % USERNAME)
+                if central_account.login(USERNAME, PASSWORD):
+                    login_via_pw = True
+            except Exception as e:
+                logger.info("Couldn't login user using username and password: %s" % e)
+
+        if not login_via_pw and not login_via_session:
+            raise Exception("Couldn't login user with either password or session")
+        
+
+    def get_followers(username):
+        """
+        Args:
+            username: a string that represents the username from which to get the followers
+        Returns:
+            A dictionary that is of the format: {username_from_arg : [follower1name, follower2name]}
+        """
+        pass
+
+    def write_followers():
+        pass
+
+
+
+    
+
+if __name__ == '__main__':
+    login_user()
+    
 
--- backend/username_processing/test.py
+++ backend/username_processing/test.py
@@ -4,10 +4,25 @@ from instagrapi.exceptions import LoginRequired
 import logging
 
 
+
+USERNAME = "steveyivicious"
+PASSWORD = "1028571DV"
+
+
+###ONLY NEEDED ONCE IN YOUR LIFETIME, BECAUSE THIS JUST MAKES "session.json"###
+def first_time_login_user():
+    cl = Client()
+    cl.login(USERNAME, PASSWORD)
+    cl.dump_settings("session.json")
+######################################
+
 # cl = Client()
-# cl.login("steveyivicious", "1028571DV*")
+# cl.login("steveyivicious", "1028571DV")
 # print(cl.user_id)
 
 # followers = cl.user_followers(cl.user_id, amount=1)
 # other_followers = cl.user_followers()
 # print(followers)
+
+###THIS WORKS AFTER YOU LOAD THE SESSION SETTINGS. NO LOGIN NEDED.
+#print(cl.user_followers("70684503354")) 
