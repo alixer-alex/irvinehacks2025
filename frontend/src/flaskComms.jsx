6404bdcaf761cf996a78dca0ec5cdc69344005d4
Alex Zhuang <azhuang3@uci.edu>
2025-01-25 16:10:59 -0500

    a

--- frontend/src/flaskComms.jsx
+++ frontend/src/flaskComms.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Graph from "graphology";
 import { SigmaContainer, useLoadGraph } from "@react-sigma/core";
 import "@react-sigma/core/lib/style.css";
@@ -7,17 +7,21 @@ const sigmaStyle = { height: "1080px", width: "1080px" };
 
 // Component that load the graph
 export const LoadGraph = () => {
-  const loadGraph = useLoadGraph();
-
-  useEffect(() => {
-    const graph = new Graph();
-
-    graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node", color: "#FA4F40" });
-    loadGraph(graph);
-  }, [loadGraph]);
-
-  return null;
-};
+    const loadGraph = useLoadGraph();
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        const graph = new Graph();
+        fetch('../../backend/mutual_followers.json')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.log(error))
+        
+        graph.addNode("first", { x: 0, y: 0, size: 15, label: "My first node", color: "#FA4F40" });
+        loadGraph(graph);
+        console.log(data);
+    }, [loadGraph]);
+    return null;
+}
 
 // Component that display the graph
 export const DisplayGraph = () => {
