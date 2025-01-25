4c60886e8fcfcca4be0cad6c4043f16bb67a07d1
Steven Jongyun Yi <sjyi1@uci.edu>
2025-01-25 11:40:25 -0800

    made all_followers.json and mutual_followers.json

--- backend/test.py
+++ backend/test.py
@@ -1,10 +1,13 @@
 
 from instagrapi import Client
-import inspect
+from instagrapi.exceptions import LoginRequired
+import logging
 
 
-cl = Client()
-cl.login("filthy_franks_partner", "1028571DV")
+# cl = Client()
+# cl.login("filthy_franks_partner", "1028571DV")
+# print(cl.user_id)
 
-followers = cl.user_followers(cl.user_id, amount=1)
-print(followers)
+# followers = cl.user_followers(cl.user_id, amount=1)
+# other_followers = cl.user_followers()
+# print(followers)
