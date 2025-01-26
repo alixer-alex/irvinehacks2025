5a2ea83a75486847cefc8f75baebec6ef57f42ad
Alex Zhuang <azhuang3@uci.edu>
2025-01-25 19:05:25 -0500

    temp graph

--- frontend/src/flaskComms.jsx
+++ frontend/src/flaskComms.jsx
@@ -13,15 +13,20 @@ export const LoadGraph = (username) => {
     const [data, setData] = useState(null);
     useEffect(() => {
         const graph = new Graph();
-        fetch('/api/'+ username.username)
-        .then(response => response.json())
-        .then(data => setData(data))
-        .catch(error => console.log(error))
+        // console.log(username.username)
+        // fetch('/api/'+ username.username)
+        // .then(response => response.json())
+        // .then(data => setData(data))
+        // .catch(error => console.log(error))
         
-        for(let key in data){
-          graph.addNode(key, { x: 0, y: 0, size: 15, label: key, color: "#FA4F40" });
-        }
-        
+        // for(let key in data){
+        //   graph.addNode(key, { x: 0, y: 0, size: 15, label: key, color: "#FA4F40" });
+        // }
+        graph.addNode("A", { x: 0, y: 0, size: 15, label: "A", color: "#FA4F40" });
+        graph.addNode("B", { x: 0, y: 1, size: 15, label: "B", color: "blue" });
+        graph.addNode("C", { x: 1, y: 0, size: 15, label: "C", color: "green" });
+        graph.addEdge("A","C")
+        graph.addEdge("B","C")
         loadGraph(graph);
         assign();
         console.log(data);
