a580df0f4bf61c857681bfaeb693f283fd071b27
Alex Zhuang <azhuang3@uci.edu>
2025-01-25 21:11:07 -0500

    maybe works?

--- backend/flask_api.py
+++ backend/flask_api.py
@@ -1,11 +1,14 @@
 
 # import time
 from flask import Flask, jsonify, request
+from flask_cors import CORS  # Import CORS
+import json
 from instagrapi import Client
 import inspect
 from username_processing import follower_processing
 
 app = Flask(__name__)
+CORS(app)
 
 # @app.route('/api/username?')
 # def get_current_time():
@@ -18,16 +21,18 @@ def get_username(username): #flask automatically calls this when a user makes an
     #just the usernames of 1 username, process it and send over to follower_processing
     # return a tuple of a blank dictionary, 200 (success code)
 	# gives one username at a time
+	
+
 	try:
 		# username captured by flask: jsonify({"username": username})
-		if len(str(username)) == 0:
+		if str(username) == 'x':
 			with open("mutual_followers.json", "r") as f:
 				data = json.load(f) 
-			return {"data":data}, 200
+			return {'data': jsonify(data)}, 200
 		all_followers = follower_processing.get_followers(str(username))
-		return {"data":jsonify(all_followers)}, 200
+		return {'data':jsonify(all_followers)}, 200
 	except Exception as ex:
-		return jsonify({"Error": str(ex)}), 400
+		return {"data":jsonify({"Error": str(ex)})}, 400
 	
 # print(get_username(Client()))
 
