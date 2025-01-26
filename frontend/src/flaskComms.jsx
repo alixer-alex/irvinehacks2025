8fd8277865b1012e791a7eec8240509c5ce337ae
Alex Zhuang <azhuang3@uci.edu>
2025-01-26 04:16:46 -0500

    testin

--- frontend/src/flaskComms.jsx
+++ frontend/src/flaskComms.jsx
@@ -12,7 +12,7 @@ export const LoadGraph = (username) => {
   const [friends, setFriends] = useState(null);
   async function getData(){
     
-    const url = "http://localhost:5000/api/" + username.username;
+    const url = "irvinehacks2025-production.up.railway.app/api/" + username.username;
     try {
       const response = await fetch(url);
       if (!response.ok) {
